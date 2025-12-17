d888172a5596bd8a70dc2eb39c4e3bd21ddb8b65
ketr1 <ketpodpischik@gmail.com>
2025-12-17 13:35:18 +0300

    1 step completed

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const { version } = require('../package.json');
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version(version);
+
+program.parse();
