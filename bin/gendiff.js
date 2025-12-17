d602ab83958c5cc7c656f9ae56e3b18891f3fcc9
ketr1 <ketpodpischik@gmail.com>
2025-12-17 13:36:34 +0300

    2 step completed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,6 +9,8 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version(version);
+  .version(version)
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
